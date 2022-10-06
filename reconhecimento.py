0d3d8d1a16bb0a0a7bf32f7f2b4c8c814398d93e
amIbono <105179649+amIbono@users.noreply.github.com>
2022-10-06 17:08:00 -0300

    adicionado uns negocio ai, n lembro oq alterou

--- reconhecimento.py
+++ reconhecimento.py
@@ -10,13 +10,22 @@ height = int(cap.get(4))
 # print(width,height)
 
 deteccoes = np.array([])
-lower_blue = np.array([116, 50,50])# array da cor mais clara time azul (alterar para a cor utilizada no robo físico)
-upper_blue = np.array([126, 255, 255])# array da cor mais escura time azul (alterar para a cor utilizada no robo físico)
+lower_blue = np.array([100, 100,100])# array da cor mais clara time azul (alterar para a cor utilizada no robo físico)
+upper_blue = np.array([150, 255, 255])# array da cor mais escura time azul (alterar para a cor utilizada no robo físico)
 lower_yellow = np.array([10, 50,50])# array da cor mais clara time amarelo (alterar para a cor utilizada no robo físico)
 upper_yellow = np.array([46, 255, 255])# array da cor mais escura time amarelo (alterar para a cor utilizada no robo físico)
 lower_teamColor = np.array([164, 50,50])# array da cor mais clara do membro do time
 upper_teamColor = np.array([174, 255, 255])# array da cor mais escura do membro do time
 
+#thresholds:
+distancia = 500 # em milimetros
+areaAzul = 18000*(100**2)/(distancia**2) # formula(d) = (areapixelsmedida*distanciamedida^2)/(d)^2
+areaRosa = areaAzul*(280/1200) # multiplica a azul pela proporção entre os retângulos pra achar a rosa
+areaRobo = areaAzul*(7412/1200) #multiplica a azul pela proporção entre os retângulos pra achar o robo inteiro
+tamanhoAumentar = int((areaRobo**(1/2))/2)
+tolerancia = 50/100
+
+print(tamanhoAumentar)
 if time == 0:
     lower_color = lower_blue
     upper_color = upper_blue
@@ -40,18 +49,22 @@ while True:# Loop de repetição para ret e frame do vídeo
     for cnt in contornos:
         #Cálculo da área e remoção de elementos pequenos
         area = cv2.contourArea(cnt)
-        if(area > 700):
+        if(areaAzul*(1-tolerancia) <= area <= areaAzul*(1+tolerancia)):
+
+            #print(area) #debug
+
             cv2.drawContours(roi, [cnt], -1, (0, 255, 0),0)
             x, y, w, h = cv2.boundingRect(cnt)
             #cv2.rectangle(roi, (x,y), (x +w, y +h), (0, 255, 0), 3)
             cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 0)
             np.append(deteccoes,[x,y,w,h])
-            usada =  roi[y-80: y+h+80,  x-80: x+w+80]
-            cv2.imshow("usada",usada)#Exibe a filmagem("ROI") do vídeo
+
+            usada = roi[max(y-tamanhoAumentar,0) : min(y+h+tamanhoAumentar,height),  max(x-tamanhoAumentar,0): min(x+w+tamanhoAumentar,width)] #clampa
             if len(usada) == 0 or len(usada[0]) == 0: # resolvendo bugs de gravação
                 break
-            # detecção do num no time
 
+            # detecção do num no time
+            cv2.imshow("usada",usada)#Exibe a filmagem("ROI") do vídeo
             hsvNum = cv2.cvtColor(usada, cv2.COLOR_BGR2HSV) # A cores em HSV funcionam baseadas em hue, no caso do opencv, varia de 0 a 180º (diferente do padrão de 360º)
             maskNum = cv2.inRange(hsvNum, lower_teamColor, upper_teamColor) # máscara para detecção de um objeto
             Num, maskNum = cv2.threshold(maskNum, 254, 255, cv2.THRESH_BINARY)
@@ -60,7 +73,7 @@ while True:# Loop de repetição para ret e frame do vídeo
             numeroNoTime = 0
             for cntNum in contornosNum:
                 areaNum = cv2.contourArea(cntNum)
-                if(areaNum > 750):
+                if(areaRosa*(1-tolerancia) <= areaNum <= areaRosa*(1+tolerancia)):
                     cv2.drawContours(usada, [cntNum], -1, (255, 255, 255),0)
                     xnum, ynum, wnum, hnum = cv2.boundingRect(cntNum)
                     #cv2.rectangle(roi, (x,y), (x +w, y +h), (0, 255, 0), 3)
@@ -70,7 +83,7 @@ while True:# Loop de repetição para ret e frame do vídeo
                     
 
     'print(deteccoes)'
-    #cv2.imshow("Mask", mask)#Exibe a máscara("Mask") do vídeo
+    cv2.imshow("Mask", mask)#Exibe a máscara("Mask") do vídeo
     cv2.imshow("ROI",roi)#Exibe a filmagem("ROI") do vídeo
     #cv2.imshow("usada",usada)#Exibe a filmagem("ROI") do vídeo
     if cv2.waitKey(25) == ord('q'):#tempo de exibição infinito (0) ou até se apertar a tecla q
