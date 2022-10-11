c8291a22fee476363f8c9ef7263ffcd5c85b3f3f
amIbono <105179649+amIbono@users.noreply.github.com>
2022-10-11 19:45:12 -0300

    adicionado cor verde para identificação de direção
    falta adicionar melhor as direções, já que só foi adicionado esquerda e direita.

--- reconhecimento.py
+++ reconhecimento.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
+import math
 
 time = 0 # 0 para time azul, 1 para time amarelo
-cap = cv2.VideoCapture(0) # Camera
+cap = cv2.VideoCapture(1) # Camera
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 width = int(cap.get(3))
@@ -10,22 +11,29 @@ height = int(cap.get(4))
 # print(width,height)
 
 deteccoes = np.array([])
-lower_blue = np.array([100, 100,100])# array da cor mais clara time azul (alterar para a cor utilizada no robo físico)
+lower_blue = np.array([100, 50,50])# array da cor mais clara time azul (alterar para a cor utilizada no robo físico)
 upper_blue = np.array([150, 255, 255])# array da cor mais escura time azul (alterar para a cor utilizada no robo físico)
-lower_yellow = np.array([10, 50,50])# array da cor mais clara time amarelo (alterar para a cor utilizada no robo físico)
+lower_yellow = np.array([26, 50,50])# array da cor mais clara time amarelo (alterar para a cor utilizada no robo físico)
 upper_yellow = np.array([46, 255, 255])# array da cor mais escura time amarelo (alterar para a cor utilizada no robo físico)
 lower_teamColor = np.array([164, 50,50])# array da cor mais clara do membro do time
 upper_teamColor = np.array([174, 255, 255])# array da cor mais escura do membro do time
-lower_ball = np.array([0, 100,100])
-upper_ball = np.array([10,255,255])
+lower_ball = np.array([0, 50,50])
+upper_ball = np.array([16,255,255])
+lower_green = np.array([80,50,50])
+upper_green = np.array([90,255,255])
 
 #thresholds:
-distancia = 500 # em milimetros
+distancia = 200 # em milimetros
 areaAzul = 18000*(100**2)/(distancia**2) # formula(d) = (areapixelsmedida*distanciamedida^2)/(d)^2
 areaRosa = areaAzul*(280/1200) # multiplica a azul pela proporção entre os retângulos pra achar a rosa
 areaRobo = areaAzul*(7412/1200) #multiplica a azul pela proporção entre os retângulos pra achar o robo inteiro
 tamanhoAumentar = int((areaRobo**(1/2))/2)
 tolerancia = 50/100
+
+# vetor direção
+normalx = np.array([width,height]) ; normaly = np.array([0,0])
+direcao1x = np.array([0,0]) ; direcao1y = np.array([0,0])
+direcao1x = np.clip(direcao1x,0,height) ; direcao1y = np.clip(direcao1x,0,height)
 
 #print(tamanhoAumentar)
 if time == 0:
@@ -46,7 +54,6 @@ while True:# Loop de repetição para ret e frame do vídeo
     hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV) # A cores em HSV funcionam baseadas em hue, no caso do opencv, varia de 0 a 180º (diferente do padrão de 360º)
     mask = cv2.inRange(hsv, lower_color, upper_color) # máscara para detecção de um objeto
     _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
-    result = cv2.bitwise_and(roi, roi, mask=mask)
     contornos, _ =  cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     maskBall = cv2.inRange(hsv,lower_ball,upper_ball)
     maskEnemy = cv2.inRange(hsv,lower_enemy,upper_enemy)
@@ -54,6 +61,27 @@ while True:# Loop de repetição para ret e frame do vídeo
     testando2, maskEnemy = cv2.threshold(maskEnemy, 254, 255, cv2.THRESH_BINARY)
     contornoBall, testando =  cv2.findContours(maskBall, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contornosEnemy, testando2 =  cv2.findContours(maskEnemy, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    maskDir = cv2.inRange(hsv,lower_green,upper_green)
+    testando3, maskDir = cv2.threshold(maskDir, 254, 255, cv2.THRESH_BINARY)
+    contornoDir, testando3 =  cv2.findContours(maskDir, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    bola = [0,0,0,0]
+
+    for cnt in contornoBall:
+        #Cálculo da área e remoção de elementos pequenos
+        area = cv2.contourArea(cnt)
+
+        if area > 100:
+
+            cv2.drawContours(roi, [cnt], -1, (0, 255, 0),0)
+            xbola, ybola, wbola, hbola = cv2.boundingRect(cnt)
+            bola[0] = xbola ; bola[1] = ybola ; bola[2] = wbola ; bola[3] = hbola
+            #cv2.rectangle(roi, (x,y), (x +w, y +h), (0, 255, 0), 3)
+            cv2.rectangle(roi, (xbola, ybola), (xbola + wbola, ybola + hbola), (0, 255, 0), 0)
+            np.append(deteccoes,[xbola,ybola,wbola,hbola])
+            roi = cv2.putText(roi,str("ball"),(xbola+40,ybola-15),font,0.8,(255,255,0),2,cv2.LINE_AA)
+    #print(bola)
 
     for cnt in contornos:
         #Cálculo da área e remoção de elementos pequenos
@@ -68,41 +96,48 @@ while True:# Loop de repetição para ret e frame do vídeo
             cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 0)
             np.append(deteccoes,[x,y,w,h])
 
-            usada = roi[max(y-tamanhoAumentar,0) : min(y+h+tamanhoAumentar,height),  max(x-tamanhoAumentar,0): min(x+w+tamanhoAumentar,width)] #clampa
-            if len(usada) == 0 or len(usada[0]) == 0: # resolvendo bugs de gravação
-                break
+            hsvNum = hsv[max(y-tamanhoAumentar,0) : min(y+h+tamanhoAumentar,height),  max(x-tamanhoAumentar,0): min(x+w+tamanhoAumentar,width)] #clampa
+            '''if len(usada) == 0 or len(usada[0]) == 0: # resolvendo bugs de gravação
+                break'''
 
             # detecção do num no time
-            cv2.imshow("usada",usada)#Exibe a filmagem("ROI") do vídeo
-            hsvNum = cv2.cvtColor(usada, cv2.COLOR_BGR2HSV) # A cores em HSV funcionam baseadas em hue, no caso do opencv, varia de 0 a 180º (diferente do padrão de 360º)
+            #cv2.imshow("usada",hsv)#Exibe a filmagem("ROI") do vídeo
+            #hsvNum = cv2.cvtColor(usada, cv2.COLOR_BGR2HSV) # A cores em HSV funcionam baseadas em hue, no caso do opencv, varia de 0 a 180º (diferente do padrão de 360º)
             maskNum = cv2.inRange(hsvNum, lower_teamColor, upper_teamColor) # máscara para detecção de um objeto
             Num, maskNum = cv2.threshold(maskNum, 254, 255, cv2.THRESH_BINARY)
-            resultNum = cv2.bitwise_and(usada, usada, mask=maskNum)
             contornosNum, Num =  cv2.findContours(maskNum, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             numeroNoTime = 0
+            maskDir = cv2.inRange(hsv,lower_green,upper_green)
+            testando3, maskDir = cv2.threshold(maskDir, 254, 255, cv2.THRESH_BINARY)
+            contornoDir, testando3 =  cv2.findContours(maskDir, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             for cntNum in contornosNum:
                 areaNum = cv2.contourArea(cntNum)
                 if(areaRosa*(1-tolerancia) <= areaNum <= areaRosa*(1+tolerancia)):
-                    cv2.drawContours(usada, [cntNum], -1, (255, 255, 255),0)
+                    cv2.drawContours(roi, [cntNum], -1, (255, 255, 255),0)
                     xnum, ynum, wnum, hnum = cv2.boundingRect(cntNum)
                     #cv2.rectangle(roi, (x,y), (x +w, y +h), (0, 255, 0), 3)
-                    cv2.rectangle(usada, (xnum, ynum), (xnum + wnum, ynum + hnum), (0, 0, 255), 3)
+                    cv2.rectangle(roi, (xnum, ynum), (xnum + wnum, ynum + hnum), (0, 0, 255), 3)
                     numeroNoTime += 1
+            for cnt in contornoDir:
+                #Cálculo da área e remoção de elementos pequenos
+                area = cv2.contourArea(cnt)
+
+                if area > 50:
+                    cv2.drawContours(roi, [cnt], -1, (0, 255, 0),0)
+                    xDir, yDir, wDir, hDir = cv2.boundingRect(cnt)
+                    #cv2.rectangle(roi, (x,y), (x +w, y +h), (0, 255, 0), 3)
+                    cv2.rectangle(roi, (xDir, yDir), (xDir + wDir, yDir + hDir), (0, 0, 0), 0)
+                    np.append(deteccoes,[xDir,yDir,wDir,hDir])
+                    roi = cv2.putText(roi,str("Dir"),(xDir+40,yDir-15),font,0.8,(255,0,255),2,cv2.LINE_AA)
+                    direcao1x  = [(x+w//2),(y+h//2)] ; direcao1y = [(bola[0]+bola[2]//2),(bola[1]+bola[3]//2)]
+                    #roi = cv2.arrowedLine(roi,direcao1x,direcao1y,[255,255,255],5)
+                    if y > yDir:
+                        print("left")
+                    elif y < yDir:
+                        print("right")
+                
             roi = cv2.putText(roi,str(numeroNoTime+1),(x+40,y-15),font,0.8,(255,255,255),2,cv2.LINE_AA)
 
-    for cnt in contornoBall:
-        #Cálculo da área e remoção de elementos pequenos
-        area = cv2.contourArea(cnt)
-
-        if area > 100:
-
-            cv2.drawContours(roi, [cnt], -1, (0, 255, 0),0)
-            x, y, w, h = cv2.boundingRect(cnt)
-            #cv2.rectangle(roi, (x,y), (x +w, y +h), (0, 255, 0), 3)
-            cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 0)
-            np.append(deteccoes,[x,y,w,h])
-            roi = cv2.putText(roi,str("ball"),(x+40,y-15),font,0.8,(255,255,0),2,cv2.LINE_AA)
-    
     for cnt in contornosEnemy:
         #Cálculo da área e remoção de elementos pequenos
         area = cv2.contourArea(cnt)
