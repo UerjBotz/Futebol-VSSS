69cb6ded573b895347500f9ae98a9bf86913517e
theo-al <emailtheoprauerj@gmail.com>
2022-11-12 14:15:42 -0300

    to-do movido para outro arquivo

--- main.py
+++ main.py
@@ -27,39 +27,6 @@ class objeto_movel : #não usada
     w: int = 0; h: int = 0
     dx:int = 0; dy:int = 0;
 
-'''
-TODO:
-- arrumação:
-    - deletar usada ou usar de verdade pra alguma coisa
-    - classe pro menu (com inseridor e mostrador de trackbar, desligador de ui)
-    - função de posição do retângulo id (recebe uma parte da tela (+ cor?))
-    - *
-    - **
-- movimento:
-    - astar
-        - ver grade direitinho (passar robôs por mais que o retângulo central)
-        - integrar no código
-    - movimentos em geral (estratégia de jogo, etc.)
-    - começar a usar o código da comunicação
-- geral:
-    - ajustar area_bola, com o tamanho esperado certo em pixels
-    - endireitar o vetor pra usar (em progresso) #(percebi que o tamanho varia de acordo com a posição na tela, investigar)
-    - vetor bola-robô
-    - adicionar outras cores e inserir no loop
-        - ajustar e pôr slider
-        - *fazer função/classe pra isso
-    - criar modos de jogo (plano: switch, argumentos de linha de comando)
-    - ver regras
-        - ver regras de cor (/+quantidade)
-    - ver membro do time e "personalidade"
-- propostas:
-    - colocar TO-DO em outro arquivo (ver)
-    - colocar menu de seleção de cores em outro módulo
-    - **fazer arquivo main e transformar reconhecimento em uma biblioteca
-    - classe janelas
-        - ***fazer funções pra escrever na tela e adicionar opção de desligar
-'''
-
 time = 0 # 0 para time azul, 1 para time amarelo
 cap = cv2.VideoCapture(0) # Camera (alterar numero caso camera esteja em outro valor)
 
