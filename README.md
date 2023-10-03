1b95637fa38972c487ffea725e3f23cd0adf2d1e
theo-al <93951769+theo-al@users.noreply.github.com>
2023-10-03 17:07:40 +0000

    Adequado o repositório ao uso da visão da RobôCIn.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Futebol-VSSS
+# Futebol-VSSS/LARC
 
-Aqui serão postos os códigos utilizados para o futebol de robôs VSSS (Very Small Size Soccer). Usem com moderação :)
+Código versão LARC.
--- comunicação.py
+++ comunicação.py
@@ -1,28 +1,34 @@
-import serial #type: ignore
+import serial  #type: ignore
 
 from time import sleep
 from numpy import ndarray, zeros
 
-master: serial.Serial
-envio: ndarray = zeros(6)
+envio = [0, 0, 0, 0, 0, 0]
+master = serial.Serial("/dev/ttyUSB0", 115200)
+sleep(3)
+master.timeout = 3
 
-def converter (pacote: ndarray) -> str :
-	return ",".join(pacote)
-	# return f"{pacote[0]},{pacote[1]},{pacote[2]},{pacote[3]},{pacote[4]},{pacote[5]}"
 
-def começar (porta:str,baudrate:int) :
-	master = serial.Serial(porta,baudrate); sleep(3)
-	master.timeout = 3
+def converter(pacote: list[int]) -> str:
+  return "Send 333, " + f"{pacote[0]},{pacote[1]},{pacote[2]},{pacote[3]},{pacote[4]},{pacote[5]}"
+  # return f"{pacote[0]},{pacote[1]},{pacote[2]},{pacote[3]},{pacote[4]},{pacote[5]}"
 
-def rodar (girar:bool) :
-	if girar == True:
-		master.write("-1000,1000,-1000,1000,-1000,1000".encode())
-	else:
-		master.write("0,0,0,0,0,0".encode())
 
-def mover (motor_esq:int, motor_dir:int,*, robo:int=0) :
-	envio[robo*2] = motor_esq; envio[robo*2 +1] = motor_dir
-	master.write(converter(envio).encode())
+def rodar(girar: bool):
+  if girar == True:
+    master.write("Send 333, -1000,1000,-1000,1000,-1000,1000".encode())
+  else:
+    master.write("Send 333, 0,0,0,0,0,0".encode())
 
-def finalizar ():
-	master.close()
+def mover(motor_esq: int, motor_dir: int, *, robo: int = 0):
+  envio[robo * 2] = motor_esq
+  envio[robo * 2 + 1] = motor_dir
+  master.write(converter(envio).encode())
+
+def enviar(velocidades: list[int]):
+  envio = velocidades
+  master.write(converter(envio).encode())
+
+def finalizar():
+  enviar([0,0,0,0,0,0])
+  master.close()
